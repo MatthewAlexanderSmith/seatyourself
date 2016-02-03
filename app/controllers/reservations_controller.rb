4e84bf43b8eeb64b013434642e65864c83887328
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-02-03 17:37:50 -0500

    Define routes, generate users, sessions, restaurants, and reservations controllers and filled out users controller

---
+++ app/controllers/reservations_controller.rb
@@ -0,0 +1,16 @@
+class ReservationsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+  before_action :load_user, only: [:show, :destroy]
+
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      redirect_to sessions_new_url, notice: "Sign Up Successful - Please Login"
+    else
+      render :new
+    end
+  end
+
+  def show
+
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+  def user_params
+    require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def load_user
+    @user = User.find(params[:id])
+  end
+end
